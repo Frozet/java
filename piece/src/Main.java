ac0b743e33740ec451e724d03176d13fd23df04b
Frozet <frozetls@gmail.com>
2017-11-18 18:02:12 +0800

    start work for piece

---
+++ piece/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    private String printTextPerRole(String[] roles, String[] textLines) {
+        return "";
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
